f17ca29c7b77a617fe6da39525f1d97e77c9c505
Oarabile Macdonald <Oarabile.Macdonald@smollan.com>
2019-09-06 09:34:58 +0200

    The Comment

---
+++ src/Executor.java
@@ -0,0 +1,10 @@
+
+public class Executor {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		  DatabaseAccessObject dao = new DatabaseAccessObject();
+		  dao.readDataBase();
+	}
+
+}
